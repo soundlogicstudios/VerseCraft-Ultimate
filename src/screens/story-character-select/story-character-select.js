2b7cd654976a1b9adede0b500d2afba6a442519f
soundlogicstudios <vyfx5g2p8f@privaterelay.appleid.com>
2026-01-07 00:27:04 -0800

    Update story-character-select.js

--- src/screens/story-character-select/story-character-select.js
+++ src/screens/story-character-select/story-character-select.js
@@ -1,5 +1,3 @@
-import './story-character-select.css';
-
 export async function createScreen({ mountEl, screenManager }) {
   const wrapper = document.createElement('div');
   wrapper.innerHTML = `<section class="screen story-character-select" data-screen="story-character-select">
