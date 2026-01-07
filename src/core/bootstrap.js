97eab2f78ce9f57cc1fce2d132ec69a215cdb5cb
soundlogicstudios <vyfx5g2p8f@privaterelay.appleid.com>
2026-01-06 21:08:32 -0800

    Update bootstrap.js

--- src/core/bootstrap.js
+++ src/core/bootstrap.js
@@ -2,10 +2,9 @@ import { screenManager } from './screen-manager.js';
 import { inputLock } from './input-lock.js';
 import { stateStore } from './state-store.js';
 
-inputLock.init();
-stateStore.init();
-
-screenManager.init({
-  mountId: 'app',
-  initialScreen: 'splash'
-});
+function showBootError(message) {
+  try {
+    const app = document.getElementById('app');
+    if (!app) return;
+    app.innerHTML = `
+      <
